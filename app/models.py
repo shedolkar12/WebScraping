e3163530fd8cf55f0e7f748541cfcbc55af940d4
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-05-08 00:22:34 +0530

    added app_details model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class app_details(models.Model):
+    app_id = models.CharField(max_length=50)
+    app_name = models.CharField(max_length=50)
+    developer = models.CharField(max_length=50)
+    developer_email = models.EmailField(max_length=50)
+    icon_url = models.CharField(max_length=100)
