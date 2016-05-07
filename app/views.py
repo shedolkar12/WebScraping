339e91ae1335163ea84126bf2e4c0e9b97b9fde7
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-05-08 00:42:58 +0530

    added required libraries

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
+from lxml import html
+import requests
+from app.models import *
+# Create your views here. 
 
 # Create your views here.
